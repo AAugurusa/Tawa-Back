faa62b173dbb44a9be536cb58bcdd0144bbe6060
Pedro <petocasti@gmail.com>
2023-03-15 15:13:58 -0300

    fifth commit 2.0
    unos cambios a rutas

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,9 +4,9 @@ import { methods as userscontroller } from "../controllers/users.controller";
 const router = Router();
 
 router.get("/", userscontroller.getUsers);
-router.get("/:nickname", userscontroller.getUser);
+router.get("/:nickname/login", userscontroller.getUser);
 router.delete("/:nickname", userscontroller.deleteUser);
 router.put("/:nickname1", userscontroller.updateUser);
-router.post("/", userscontroller.addUser);
+router.post("/signUp", userscontroller.addUser);
 
 export default router;
