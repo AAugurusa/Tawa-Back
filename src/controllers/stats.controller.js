7bea17221e2e1c6865cf02aaee85edd81ac8ea4b
Pedro <petocasti@gmail.com>
2023-05-10 11:52:37 -0300

    Updates correctos
    jejeje

--- src/controllers/stats.controller.js
+++ src/controllers/stats.controller.js
@@ -29,28 +29,112 @@ const setStatsTo0 = async (req,res)=>{
     }
 };
 
+const updateT_merge = async (req, res) => {
+  try {
+    const { nickname } = req.params;
+    const { t_merge } = req.body;
+    const connection = await getConnection();
+    const body = await connection.query("SELECT iduser FROM users WHERE nickname = ?", [nickname]);
+    const statsID = await connection.query("SELECT idstats FROM stats WHERE iduser = ?", [body[0].iduser]);
+    const result = await connection.query("UPDATE stats SET t_merge = t_merge + ? WHERE idstats = ?", [t_merge, statsID[0].idstats]);
+    res.status(201).json("Success");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json("Error");
+  }
+};
 
 
-const updateStats = async (req, res) => {
-    try {
-      const { nickname } = req.params;
-      const { t_merge, t_buy, enemy_kills, cur_spent, high_score, game_time } = req.body;
-      const connection = await getConnection();
-      const userID = await connection.query("SELECT iduser FROM users WHERE nickname = ?", [nickname]);
-      const statsID = await connection.query("SELECT idstats FROM stats WHERE iduser = ?", [userID[0].iduser]);
-      const result = await connection.query(
-        "UPDATE stats SET t_merge = t_merge + ?, t_buy = t_buy + ?, enemy_kills = enemy_kills + ?, cur_spent = cur_spent + ?, high_score = ?, game_time = game_time + ? WHERE idstats = ?",
-        [t_merge, t_buy, enemy_kills, cur_spent, high_score, game_time,statsID[0].idstats]
-      );
-      res.json(result);
-    } catch (error) {
-      res.status(500); // indicates an error in the server request, hence "500"
-      res.send(error.message);
+const updateT_buy = async (req,res)=>{
+  try{
+    const { nickname } = req.params;
+    const { t_buy } = req.body;
+    const connection = await getConnection();
+    const body = await connection.query("SELECT iduser FROM users WHERE nickname = ?", [nickname]);
+    const statsID = await connection.query("SELECT idstats FROM stats WHERE iduser = ?", [body[0].iduser]);
+    const result = await connection.query("UPDATE stats SET t_buy = t_buy + ? WHERE idstats = ?", [t_buy, statsID[0].idstats]);
+    res.status(201).json("Success");
+  }catch(error){
+    console.log(error);
+    res.status(500).json("Error");
+  }
+};
+
+const updateEnemy_kills = async (req,res)=>{
+  try{
+    const { nickname } = req.params;
+    const { enemy_kills } = req.body;
+    const connection = await getConnection();
+    const body = await connection.query("SELECT iduser FROM users WHERE nickname = ?", [nickname]);
+    const statsID = await connection.query("SELECT idstats FROM stats WHERE iduser = ?", [body[0].iduser]);
+    const result = await connection.query("UPDATE stats SET enemy_kills = enemy_kills + ? WHERE idstats = ?", [enemy_kills, statsID[0].idstats]);
+    res.status(201).json("Success");
+  }catch(error){
+    console.log(error);
+    res.status(500).json("Error");
+  }
+};
+
+const updateCur_spent = async (req,res)=>{
+  try{
+    const { nickname } = req.params;
+    const { cur_spent } = req.body;
+    const connection = await getConnection();
+    const body = await connection.query("SELECT iduser FROM users WHERE nickname = ?", [nickname]);
+    const statsID = await connection.query("SELECT idstats FROM stats WHERE iduser = ?", [body[0].iduser]);
+    const result = await connection.query("UPDATE stats SET cur_spent = cur_spent + ? WHERE idstats = ?", [cur_spent, statsID[0].idstats]);
+    res.status(201).json("Success");
+  }catch(error){
+    console.log(error);
+    res.status(500).json("Error");
+  }
+};
+
+const updateHigh_score = async (req,res)=>{
+  try{
+    const { nickname } = req.params;
+    const { high_score } = req.body;
+    const connection = await getConnection();
+    const body = await connection.query("SELECT iduser FROM users WHERE nickname = ?", [nickname]);
+    const statsID = await connection.query("SELECT idstats FROM stats WHERE iduser = ?", [body[0].iduser]);
+    const prev_HS = await connection.query("SELECT high_score FROM stats WHERE idstats = ?", [statsID[0].idstats]);
+    if(prev_HS[0].high_score < high_score){
+      const result = await connection.query("UPDATE stats SET high_score = ? WHERE idstats = ?", [high_score, statsID[0].idstats]);
+      res.status(201).json("Success");
     }
-  };
+    else{
+      res.status(201).json("Did not beat high score");
+    }
+  }catch(error){
+    console.log(error);
+    res.status(500).json("Error");
+  }
+};
+
+const updateGame_time = async (req,res)=>{
+  try{
+    const { nickname } = req.params;
+    const { game_time } = req.body;
+    const connection = await getConnection();
+    const body = await connection.query("SELECT iduser FROM users WHERE nickname = ?", [nickname]);
+    const statsID = await connection.query("SELECT idstats FROM stats WHERE iduser = ?", [body[0].iduser]);
+    console.log(statsID[0].idstats);
+    const result = await connection.query("UPDATE stats SET game_time = game_time + ? WHERE idstats = ?", [game_time, statsID[0].idstats]);
+    res.status(201).json("Success");
+  }catch(error){
+    console.log(error);
+    res.status(500).json("Error");
+  }
+};
+
 
 export const methods = {
     createStats,
     setStatsTo0,
-    updateStats
+    updateT_merge,
+    updateT_buy,
+    updateEnemy_kills,
+    updateCur_spent,
+    updateHigh_score,
+    updateGame_time
 };
--- src/routes/stats.routes.js
+++ src/routes/stats.routes.js
@@ -4,6 +4,12 @@ import { methods as statscontroller } from "../controllers/stats.controller";
 const router = Router();
 router.post("/signup", statscontroller.createStats);
 router.put("/signup", statscontroller.setStatsTo0);
-router.put("/update/:nickname", statscontroller.updateStats);
+router.put("/updateT_merge/:nickname", statscontroller.updateT_merge);
+router.put("/updateT_buy/:nickname", statscontroller.updateT_buy);
+router.put("/updateEnemy_kills/:nickname", statscontroller.updateEnemy_kills);
+router.put("/updateCur_spent/:nickname", statscontroller.updateCur_spent);
+router.put("/updateHigh_score/:nickname", statscontroller.updateHigh_score);
+router.put("/updateGame_time/:nickname", statscontroller.updateGame_time);
+
 
 export default router;
